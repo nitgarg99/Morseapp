6ed8b8122624c0c3c4869dd8aed499d3aef42de1
nigarg99 <nitgarg99@gmail.com>
2018-08-28 00:02:36 -0700

    Navigation initial setup

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Alert, Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { AppRegistry, Alert, Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 class MorseCity extends Component {
     _onPressButton() {
-        Alert.alert('Button Pressed!');
+        this.props.navigation.navigate('Message');
     }
 
     render() {
@@ -14,7 +14,7 @@ class MorseCity extends Component {
                     <View style={{flex: 9, backgroundColor: '#987fba'}}>
                     </View>
                     <View style={{flex: 1, backgroundColor: '#ffffff', }}>
-                        <TouchableOpacity style={{flex:1}}>
+                        <TouchableOpacity style={{flex:1}} onPress={this._onPressButton.bind(this)}>
                             <Image resizeMode='contain' source={require('./images/message.png')} style={styles.icon} />
                         </TouchableOpacity>
                     </View>
@@ -27,15 +27,20 @@ class MorseCity extends Component {
 }
 
 class MessageScreen extends Component {
+    _onPressButton() {
+        Alert.alert('Tap!');
+    }
     render() {
         return (
-            <SafeAreaView style={{flex: 1, backgroundColor: '#51667a', justifyContent: 'center', alignItems: 'center'}}>
-                <View style={{alignSelf: 'center', alignItems: 'center', justifyContent: 'center', position: 'absolute'}} >
-                    <Text style={{flex: 1,}}>
-                        Tap anywhere on screen
-                    </Text>
-                </View>
-            </SafeAreaView>
+            <TouchableOpacity style={{flex: 1}} onPress={this._onPressButton}>
+                <SafeAreaView style={{flex: 1, backgroundColor: '#51667a', justifyContent: 'center', alignItems: 'center'}}>
+                    <View style={{alignSelf: 'center', alignItems: 'center', justifyContent: 'center', position: 'absolute'}} >
+                        <Text style={{flex: 1,}}>
+                            Tap anywhere on screen
+                        </Text>
+                    </View>
+                </SafeAreaView>
+            </TouchableOpacity>
         );
     }
 }
@@ -46,7 +51,7 @@ const RootStack = createStackNavigator(
     Message: MessageScreen,
     },
     {
-        initialRouteName: 'Message',
+        initialRouteName: 'Home',
     }
 );
 
@@ -69,3 +74,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#abb2b9',
     },
 });
+
+AppRegistry.registerComponent('morseapp', () => RootStack);
