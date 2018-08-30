4b80cdc0d7c048cb3d3b2df4059c0769c73a2c9f
nigarg99 <nitgarg99@gmail.com>
2018-08-30 13:11:52 -0700

    User auth

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry, Alert, Button, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import firebase from 'firebase';
 
 class MorseCity extends Component {
     _onPressButton() {
@@ -38,14 +39,20 @@ class LoginScreen extends Component {
     _onPressSubmit() {
 
     } 
+    _onPressSignup() {
+        this.props.navigation.navigate('Signup');
+    }
     render() {
         return (
             <SafeAreaView style={{flex: 1, backgroundColor: '#ffffff', justifyContent: 'center', alignItems: 'center'}}>
                 <View style={{flex: 1, backgroundColor: '#fff', justifyContent: 'center', position: 'absolute'}} >
-                    <TextInput onChangeText={(text) => this.setState({username: text})} value={this.state.username}  placeholder='Username'/>
-                    <TextInput onChangeText={(text) => this.setState({password: text})} value={this.state.password}  placeholder='Password' secureTextEntry={true}/>
+                    <TextInput onChangeText={(username) => this.setState({username})} value={this.state.username}  placeholder='Username'/>
+                    <TextInput onChangeText={(password) => this.setState({password})} value={this.state.password}  placeholder='Password' secureTextEntry={true}/>
                     <TouchableOpacity style={{flex:1}} onPress={this._onPressSubmit.bind(this)}>
                         <Text>Submit</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{flex:1}} onPress={this._onPressSignup.bind(this)}>
+                        <Text>Sign up</Text>
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
@@ -53,6 +60,42 @@ class LoginScreen extends Component {
     }
 }
 
+class SignupScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            username: '', 
+            password: '',
+            errorMessage: '',
+
+        };
+    }
+    _onPressSignup() {
+        this.setState({errorMessage: ''});
+        firebase.auth().createUserWithEmailAndPassword(this.state.username, this.state.password).catch((error) => {
+            this.setState({errorMessage: error.message})
+        });
+
+    } 
+    render() {
+        return (
+            <SafeAreaView style={{flex: 1, backgroundColor: '#ffffff', justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{flex: 1, backgroundColor: '#fff', justifyContent: 'center', position: 'absolute'}} >
+                    <View style={{flex: 1, backgroundColor: '#fff', justifyContent: 'center', }} >
+                        <TextInput onChangeText={(username) => this.setState({username})} value={this.state.username}  placeholder='Username'/>
+                        <TextInput onChangeText={(password) => this.setState({password})} value={this.state.password}  placeholder='Password' secureTextEntry={true}/>
+                        <TouchableOpacity style={{flex:1}} onPress={this._onPressSignup.bind(this)}>
+                            <Text>Sign up</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <Text>{this.state.errorMessage}</Text>
+                    </View>
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
 class MessageScreen extends Component {
     _onPressButton() {
         Alert.alert('Tap!');
@@ -74,17 +117,26 @@ class MessageScreen extends Component {
 
 const RootStack = createStackNavigator(
     {
-    Home: MorseCity,
-    Message: MessageScreen,
-    Login: LoginScreen,
+        Home: MorseCity,
+        Message: MessageScreen,
+        Login: LoginScreen,
+        Signup: SignupScreen,
     },
     {
         initialRouteName: 'Login',
     }
 );
 
+const config = {
+    apiKey: "AIzaSyDLasnATBHpxYFBzM101QWT1q2clHjQTcg",
+    authDomain: "morseapp-f0c08.firebaseapp.com",
+    databaseURL: "https://morseapp-f0c08.firebaseio.com",
+};
+
 export default class App extends Component {
+
     render() {
+        firebase.initializeApp(config);
         return <RootStack />;
     }
 }
